1ba60c3141e09faa90810d557464fa8f4f034385
Bill Kudo <bluesky42624@gmail.com>
2019-08-16 22:16:30 -0700

    Added aliases for extensions

--- supersonic/exts/__init__.py
+++ supersonic/exts/__init__.py
@@ -6,3 +6,10 @@ from supersonic.exts.bar import Bar
 from supersonic.exts.eta import Eta
 from supersonic.exts.percentage import Percentage
 from supersonic.exts.identity import Total, Current, Fraction
+
+B = Bar
+E = Eta
+P = Percentage
+T = Total
+C = Current
+F = Fraction
